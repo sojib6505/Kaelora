d760438ae3ab32d2877c70b8b67ef6e1afbbbd11
Md Sojib Islam <153745122+sojib6505@users.noreply.github.com>
2026-09-06 06:52:27 +0600

    Fix import path for CategoryProducts component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import HeroSlider from "../../components/hero/HeroSlider";
 import NewArrivals from "../../components/newArrivals/NewArrivals";
 import ScrollToTop from "../../components/scrollToTop/ScrollToTop";
 import Features from "../../components/features/Features";
-import CategoryProducts from "../CategoryProducts/CategoryProducts";
+import CategoryProducts from "../categoryProducts/CategoryProducts";
 
 
 export default function Home() {
